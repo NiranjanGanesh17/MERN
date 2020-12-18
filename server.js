456f66efe00da0467b66e1fbaa71da671c81f6aa
NiranjanGanesh17 <niranjanmech17@gmail.com>
2020-12-18 20:22:31 +0530

    version1.0.1

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ db.once('open', function () {
 //auto build for production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('recipeapp/build'));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'recipeapp', 'build', 'index.html'))
-    // })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'recipeapp', 'build', 'index.html'))
+    })
 }
 
 
